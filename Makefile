c79a4fbb265386f5809546abb3fbeece6c21ee57
Epicurus Tristana <etristan@ox-p2.21-school.ru>
2019-07-23 13:37:01 +0300

    fix Makefile and dirs

--- Makefile
+++ Makefile
@@ -6,25 +6,25 @@
 #    By: oleland <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/07/22 18:35:43 by oleland           #+#    #+#              #
-#    Updated: 2019/07/23 12:38:39 by etristan         ###   ########.fr        #
+#    Updated: 2019/07/23 13:30:20 by etristan         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 CC = gcc
 
-CFLAGS = -Wall -Wextra -Werror
+CFLAGS = -Wall -Wextra -Werror -I $(HFILES)
 
 NAME = bsq
 
-CFILES = $(wildcard *.c)
+CFILES = $(wildcard srcs/*.c)
 
-HFILES = bsq.h
+HFILES = includes/
 
 OFILES = $(CFILES:.c=.o)
 
 all: $(CFILES) $(NAME)
 $(NAME): $(OFILES)
-	$(CC) $(CFLAGS) $^ -o $@
+	$(CC) $(CFLAGS)	-o $@ $^
 
 clean:
 	/bin/rm -rf $(OFILES)
---
+++ includes/bsq.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bsq.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oleland <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/22 17:42:36 by oleland           #+#    #+#             */
+/*   Updated: 2019/07/22 21:31:12 by oleland          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BSQ_H
+# define BSQ_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+int		valid(char **argv);
+int		is_x(char **argv, int y, char pod);
+int		is_y(char **argv, int x, char pod);
+int		is_norm_square(char **argv, int yi, int xi, char pod);
+int		is_square(char **argv, int y, int x, char pod);
+int		can_use(char **argv, int y, int x, char pod);
+int		rec_sol(char ***argv, int y, int x);
+void	print_sudoku(char **argv);
+int		solution(char ***argv);
+int		sprt_rec_sol(int *x, int *y);
+
+int		g_freecount;
+
+#endif
